ea9ffccfe49b7b32ab1127c76d0d735d9bdef55a
desshub <desshub95@gmail.com>
2018-06-23 16:16:00 +0300

    [Feature #158489845] Create Signup Function

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,5 +1,49 @@
 window.onload = function() {
 
-    // POST login
+    // POST user signup
+    let elemnt = document.getElementById('signup')
+    if (elemnt){
+        elemnt.addEventListener
+        ('submit', signup);
+    }
+
+    function signup(e){
+        e.preventDefault();
     
+        let username = document.getElementById('username').value;
+        let email = document.getElementById('email').value;        
+        let password = document.getElementById('password').value;
+        let cnfpass = document.getElementById('cnfpass').value;
+    
+        fetch('https://mtracker28.herokuapp.com/api/v2/auth/register', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-type':'application/json'
+            },
+            body:JSON.stringify({username:username, email:email,
+                password:password, cnfpass:cnfpass})
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.message === "Registered Successfully"){
+                    if(data.response == "Username Is already taken"){
+                        document.getElementById('flash').style.color = 'red'
+                        document.getElementById('flash').innerHTML = data.response
+                    }else {
+                        window.location.href = 'login.html'
+                        document.getElementById('flash').style.color = 'green'
+                        document.getElementById('flash').innerHTML = data.message
+                    }
+                }else{
+                    document.getElementById('flash').style.color = 'red'
+                    document.getElementById('flash').innerHTML = data.message
+
+                }
+            })
+    
+        }
+
+
+
 }
